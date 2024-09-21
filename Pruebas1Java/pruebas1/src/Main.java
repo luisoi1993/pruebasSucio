f3384b413ed2e91bb6fba688c47db51878e09e68
Luis <luisaguadodiego1993@gmail.com>
2024-09-21 23:03:37 +0200

    primera prueba

---
+++ Pruebas1Java/pruebas1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("SKINHEADS CONTROL THE  world!");
+        System.out.println("Digite el nombre del skinhead");
+        String skinhead = entrada.nextLine();
+        System.out.println("EL nombre del skinhead es: "+skinhead);
+    }
+}
