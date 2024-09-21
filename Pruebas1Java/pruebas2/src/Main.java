e8fad288b975414f2a91250189b8332b573a3e39
luisoi1993 <luisaguadodiego1993@gmail.com>
2024-09-21 23:18:32 +0200

    prueba cambiar desde github

--- Pruebas1Java/pruebas2/src/Main.java
+++ Pruebas1Java/pruebas2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("SKINHEADS CONTROL THE  world!");
-        System.out.println("Digite el nombre del skinhead");
+        System.out.println("Digite el nombre del oi");
         String skinhead = entrada.nextLine();
         System.out.println("EL nombre del skinhead es: "+skinhead);
     }
